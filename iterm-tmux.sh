50c3ae0c0c2d7d873cf9602c59551be0ac79597b
Robert Balicki <robert.balicki@gmail.com>
2026-02-06 02:31:51 -0500

    Layer N: add n->Cmd+Space, fix iterm-tmux.sh syntax

--- iterm-tmux.sh
+++ iterm-tmux.sh
@@ -4,16 +4,19 @@
 
 WINDOW_CHAR="$1"
 
-osascript <<EOF
-tell application "iTerm" to activate
-delay 0.1
-tell application "System Events"
-    keystroke "2" using command down
-    delay 0.05
-    keystroke "1" using {command down, option down}
-    delay 0.05
-    keystroke "a" using control down
-    delay 0.05
-    keystroke "$WINDOW_CHAR"
-end tell
+osascript - "$WINDOW_CHAR" <<'EOF'
+on run argv
+    set windowChar to item 1 of argv
+    tell application "iTerm" to activate
+    delay 0.1
+    tell application "System Events"
+        keystroke "2" using command down
+        delay 0.05
+        keystroke "1" using {command down, option down}
+        delay 0.05
+        keystroke "a" using control down
+        delay 0.05
+        keystroke windowChar
+    end tell
+end run
 EOF
