83ecf8f5f8edfe0381b31c1a664086c8cae83b7b
Robert Balicki <robert.balicki@gmail.com>
2026-02-07 03:31:08 -0500

    Use pbpaste instead of Cmd+V for reliability

--- vscode-open-in-chrome.sh
+++ vscode-open-in-chrome.sh
@@ -3,42 +3,32 @@
 
 # First, copy relative path (Shift+Alt+Cmd+C)
 osascript -e 'tell app "System Events" to keystroke "c" using {shift down, option down, command down}'
-sleep 0.5
+sleep 0.3
+
+# Get clipboard contents
+FILEPATH=$(pbpaste)
 
 # Check project mode
 PROJECT=$(cat /tmp/karabiner-project 2>/dev/null)
 
 if [ "$PROJECT" = "pin" ]; then
-    # Open Work Chrome profile
+    PREFIX="cc"
     /Users/rbalicki/code/voicemode/chrome-tab.sh work
-    sleep 0.2
-    # New tab, type cc + paste + enter
-    osascript <<EOF
-tell application "System Events"
-    keystroke "t" using command down
-    delay 0.1
-    keystroke "cc"
-    keystroke "v" using command down
-    delay 0.05
-    key code 36
-end tell
-EOF
 else
-    # Open Personal Chrome profile
+    PREFIX="isof"
     /Users/rbalicki/code/voicemode/chrome-tab.sh personal
-    sleep 0.2
-    # New tab, type isof + paste + enter
-    osascript <<EOF
+fi
+
+sleep 0.2
+# New tab, type prefix + filepath + enter
+osascript <<EOF
 tell application "System Events"
     keystroke "t" using command down
     delay 0.1
-    keystroke "isof"
-    delay 0.1
-    keystroke "v" using command down
+    keystroke "${PREFIX}${FILEPATH}"
     delay 0.05
     key code 36
 end tell
 EOF
-fi
 
 echo > /tmp/karabiner-layer
